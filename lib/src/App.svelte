63ccf19f0c58d2f6e0abe107be683370ef0569f9
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2023-02-21 15:52:47 +0800

    added svelte with link to playground

---
+++ lib/src/App.svelte
@@ -0,0 +1,24 @@
+<svelte:options tag="neftyblocks-blend" />
+
+<script lang="ts">
+    import './lib/Counter.svelte';
+</script>
+
+<main>
+    <h1>Blend</h1>
+
+    <neftyblocks-counter />
+</main>
+
+<style lang="scss">
+    @import './global.scss';
+
+    :host {
+        display: block;
+        color: var(--nefty-color, #fcfcfd);
+    }
+
+    :host h1 {
+        font-size: 34px;
+    }
+</style>
