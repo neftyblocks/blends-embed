f9f05937a0580ffd84a5db8dab686aa6bc10b41b
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2023-02-21 13:46:03 +0800

    setup project

---
+++ playground/src/main.ts
@@ -0,0 +1,10 @@
+import './style.scss';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Blend embed</h1>
+    <p class="read">
+      Nothing to see here yet.
+    </p>
+  </div>
+`;
