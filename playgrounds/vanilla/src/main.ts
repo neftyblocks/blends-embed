27beec9e6674d1f0e59b5f3214c9169ed56c5bf6
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2023-03-17 16:55:20 +0800

    testnet for playground

--- playgrounds/vanilla/src/main.ts
+++ playgrounds/vanilla/src/main.ts
@@ -16,9 +16,9 @@ const mountEl = () => {
         el.innerHTML = '';
 
         const config = {
-            atomic_url: 'https://aa.neftyblocks.com',
-            chain_url: 'https://wax.neftyblocks.com',
-            marketplace_url: 'https://neftyblocks.com/marketplace/listing',
+            atomic_url: 'https://aa-testnet.neftyblocks.com',
+            chain_url: 'https://waxtest.eu.eosamsterdam.net',
+            marketplace_url: 'https://test.neftyblocks.com/marketplace/listing',
             chain: 'wax',
             collection,
         };
@@ -87,8 +87,8 @@ const callback = (users: WalletUser[]): void => {
 };
 
 useWallet({
-    chain_url: 'https://wax.neftyblocks.com',
-    chain_id: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
+    chain_url: 'https://waxtest.eu.eosamsterdam.net',
+    chain_id: 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12',
     callback,
 });
 
