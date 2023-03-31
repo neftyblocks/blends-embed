184fd02a9aa640acfce809c5859cc6bf152a5ea7
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2023-03-31 11:44:27 +0800

    added vue playground

--- playgrounds/vue/src/App.vue
+++ playgrounds/vue/src/App.vue
@@ -1,30 +1,145 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <main>
+        <header>
+            <button @click="login" class="btn">Login</button>
+            <router-link class="btn" to="/">Home</router-link>
+            <router-link class="btn" to="/alpacaworlds"
+                >Alpacaworlds</router-link
+            >
+            <router-link class="btn" to="/coffeeking13"
+                >The coffee king</router-link
+            >
+        </header>
+        <router-view></router-view>
+    </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { WalletUser } from '@nefty/use/dist/wallet';
+import { useWallet } from './composables/useWallet';
+import { user } from './composables/states';
+
+const setUser = () => {
+    user.value = JSON.stringify({
+        actor: window.provider_user.accountName,
+        permission: window.provider_user.requestPermission || 'active',
+    });
+};
+
+const login = () => {
+    window.provider.loginUser(window.wallet_anchor);
+};
+
+const callback = (users: WalletUser[]): void => {
+    const [walletUser] = users;
+
+    window.provider_user = walletUser;
+    window.provider_user.requestPermission =
+        walletUser.requestPermission || 'active';
+
+    setUser();
+
+    const anchorModal = document.querySelector('.anchor-link-active');
+    if (anchorModal) anchorModal.classList.remove('anchor-link-active');
+};
+
+useWallet({
+    chain_url: 'https://waxtest.eu.eosamsterdam.net',
+    chain_id:
+        'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12',
+    callback,
+});
+</script>
+
+<style lang="scss">
+:root {
+    --nefty-font-family: 'DM Sans', sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+body {
+    display: block;
+    box-sizing: border-box;
+    min-height: 100vh;
+    height: 100%;
+    padding: 20px;
+    margin: 0;
+
+    * {
+        font-family: var(--nefty-font-family);
+        margin: 0;
+        line-height: 1.2;
+    }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+h1 {
+    font-size: 34px;
+    text-align: center;
+}
+
+p {
+    font-size: 16px;
+    color: #666;
+    text-align: center;
+}
+
+#neftyblocks-blend {
+    padding: 20px;
+    max-width: 1520px;
+    margin: 20px auto 0;
+}
+
+header {
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    margin: 20px auto;
+    gap: 10px;
+}
+
+form {
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    width: fit-content;
+    gap: 20px;
+
+    div {
+        display: flex;
+        flex-direction: column;
+    }
+
+    label {
+        font-size: 14px;
+        color: #666;
+        margin-bottom: 5px;
+    }
+
+    input {
+        padding: 12px 24px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-size: 14px;
+        color: #eaeaea;
+        background-color: #3b3b3b;
+
+        &[type='submit'] {
+            background-color: #949494;
+            cursor: pointer;
+        }
+    }
+}
+
+.btn {
+    padding: 12px 24px;
+    border: 1px solid #233059;
+    border-radius: 5px;
+    font-size: 14px;
+    color: #eaeaea;
+    background-color: #131b33;
+    cursor: pointer;
+
+    &.hidden {
+        display: none;
+    }
 }
 </style>
---
+++ playgrounds/vue/src/pages/collection.vue
@@ -0,0 +1,66 @@
+<template>
+    <main>
+        <neftyblocks-blend
+            ref="blendEl"
+            :config="setup"
+            :account="JSON.stringify(user)"
+            :transactionId="transactionId"
+            @sign="signHandler"
+        />
+    </main>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+import { user } from '../composables/states';
+
+import '@neftyblocks/blends';
+
+const { currentRoute } = useRouter();
+const collection = currentRoute.value.params.collection as string;
+
+const blendEl = ref<HTMLElement | null>(null);
+const transactionId = ref<string | null>(null);
+const setup = ref<any | null>(
+    JSON.stringify({
+        atomic_url: 'https://aa-testnet.neftyblocks.com',
+        chain_url: 'https://waxtest.eu.eosamsterdam.net',
+        marketplace_url: 'https://test.neftyblocks.com/marketplace/listing',
+        chain: 'wax',
+        collection: collection,
+        profile_url: 'https://test.neftyblocks.com/profile/',
+        back_btn: false,
+    })
+);
+
+// watch(
+//     user,
+//     (value) => {
+//         if (value) {
+//             blendEl.value?.setAttribute('account', value);
+//         }
+//     }
+// );
+
+const signHandler = async ({ detail }: any) => {
+    console.log('signHandler', detail);
+
+    try {
+        const result = await window.provider_user.signTransaction(
+            { actions: detail },
+            {
+                broadcast: true,
+                blocksBehind: 3,
+                expireSeconds: 120,
+            }
+        );
+
+        if (result) {
+            transactionId.value = result.transaction_id;
+        }
+    } catch (error) {
+        console.log('error', error);
+    }
+};
+</script>
